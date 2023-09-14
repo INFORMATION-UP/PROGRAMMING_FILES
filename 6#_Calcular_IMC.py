db652be78318c87acf1647ec5b4660e627b5400c
INFORMATION-UP <143146189+INFORMATION-UP@users.noreply.github.com>
2023-09-14 15:55:44 -0500

    Códigos De Práctica
    Resumen: Este código en Python le pide al usuario que ingrese su peso (en kilogramos) y su altura (en metros). Luego, calcula el Índice de Masa Corporal (IMC) utilizando la fórmula estándar del IMC, que relaciona el peso y la altura. Finalmente, muestra el resultado del IMC en la pantalla.

---
+++ 6#_Calcular_IMC.py
@@ -0,0 +1,19 @@
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+#▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▶EJERCICIO #6◀▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+#REALIZAR UN PROGRAMA QUE CALCULE EL IMC (INDICE DE MASA CORPORAL)
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+
+#CÓDIGO:
+peso = float(input("Ingrese su peso en kg: "))
+altura = float(input("Ingrese su altura en metros: "))
+imc = peso / (altura ** 2) #FORMULA DEL IMC
+
+print("Su IMC es:", imc)
+
+
+
+
+
+
+
