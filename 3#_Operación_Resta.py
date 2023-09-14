27a7f1ecea986ce0b8f91849a6c217f7df276ee6
INFORMATION-UP <143146189+INFORMATION-UP@users.noreply.github.com>
2023-09-14 14:49:24 -0500

    Códigos De Aprendizaje
    Resumen: Este código en Python realiza una resta de tres números (18, 20 y 15) y luego muestra el resultado (-17) en la pantalla.

---
+++ 3#_Operación_Resta.py
@@ -0,0 +1,30 @@
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+#▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▶EJERCICIO #3◀▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+#En Matemática conocemos operaciones de suma, resta, multiplicación y división, 
+#ya sea con números enteros o fraccionarios, 
+#donde se obtiene un nuevo elemento a partir de dos elementos dados.
+
+#La resta o sustracción es una operación matemática que consiste en sacar, 
+#quitar, reducir o separar algo de un todo. Restar es una de las operaciones 
+#básicas de las matemáticas,junto a la suma, que es su proceso inverso.
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+
+#CÓDIGO:
+num1 = 18
+num2 = 20
+num3 = 15
+resultado = num1 - num2 - num3
+print("La respuesta de la resta es:" , resultado) 
+
+#LEY DE LOS SIGNOS  (SUMA Y RESTA)
+
+#+ * + = +
+#+ * - = -
+#- * + = -
+#- * - = +
+
+#18 - 20 = -2 #explicacion hecha
+#-2 - 15 = -17 
+
+
