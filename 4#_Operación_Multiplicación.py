d6f091b86dd07aca69886e17303f6cc4e5ecdad5
INFORMATION-UP <143146189+INFORMATION-UP@users.noreply.github.com>
2023-09-14 15:09:13 -0500

    Códigos De Práctica
    Resumen: Este código en Python multiplica cuatro números (5, 2, 3 y 3) y luego muestra el resultado (90) en la pantalla.

---
+++ 4#_Operación_Multiplicación.py
@@ -0,0 +1,31 @@
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+#▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▶EJERCICIO #4◀▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+#En Matemática conocemos operaciones de suma, resta, multiplicación y 
+#división,ya sea con números enteros o fraccionarios,donde se obtiene 
+#un nuevo elemento a partir de dos elementos dados. 
+
+#Multiplicar es una operación más eficaz que sumar muchas veces el 
+#mismo número. La palabra multiplicación viene del latín 
+#multiplicatio, que significa «aumentar el número de la misma cosa
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+
+#CÓDIGO:
+numer1 = 5
+numer2 = 2
+numer3 = 3
+numer4 = 3
+resultado = numer1 * numer2 * numer3 * numer4
+print("El resultado de la multiplicacion es:" , resultado)
+
+#LEY DE LOS SIGNOS (MULTIPLICACION)
+
+#(+)(+) = +
+#(+)(-) = -
+#(-)(+) = -
+#(-)(-) = +
+
+#explicacion de manera eficaz
+2*2*2*2 = 16
+4*4= 16
+8*2= 16
