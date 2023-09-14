f7e76d97e5581e2538a25922def88e0ce78c1ddc
INFORMATION-UP <143146189+INFORMATION-UP@users.noreply.github.com>
2023-09-14 15:36:50 -0500

    Códigos De Práctica
    Resumen: Este código en Python realiza una división de dos números (10 y 2) y luego muestra el resultado (5) como un número entero en la pantalla.

---
+++ 5#_Operación_División.py
@@ -0,0 +1,33 @@
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+#▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▶EJERCICIO #5◀▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+#En Matemática conocemos operaciones de suma, resta, multiplicación y división, 
+#ya sea con números enteros o fraccionarios, 
+#donde se obtiene un nuevo elemento a partir de dos elementos dados. 
+
+#La división es aquella operación matemática mediante la cual se trata 
+#de descomponer un número, al que denominaremos dividendo, en tantas 
+#partes como así lo indique otro número, al que llamaremos divisor.
+#Imaginemos que tenemos la siguiente división:
+#72÷9=8
+#Esto significa que el número 72 es igual a 8 veces el número 9 (o 9 veces el número 8). 
+#También que el número 72 se puede descomponer en 9 partes de 8 unidades cada una, 
+#u 8 partes de 9 unidades cada una.
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+
+#CÓDIGO:
+#Enteros con la funcion del int --> enteros resultado
+variable1 = 10
+variable2 = 2
+resultado = variable1 / variable2
+resultado_entero = int(resultado)
+print("Resultado:", resultado_entero)
+
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+
+#Decimales
+variable1 = 10
+variable2 = 2
+resultado = variable1 / variable2
+print("El resultado de la divison es:", resultado)
+
