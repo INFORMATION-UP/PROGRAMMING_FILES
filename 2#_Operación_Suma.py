8ff2b5731b607590d864f01d235cb4677cf2aa28
INFORMATION-UP <143146189+INFORMATION-UP@users.noreply.github.com>
2023-09-14 14:43:16 -0500

    Add files via upload
    Resumen: Este código en Python realiza una suma simple de cuatro números (5, 50, 10 y 3) y luego muestra el resultado (68) en la pantalla.

---
+++ 2#_Operación_Suma.py
@@ -0,0 +1,25 @@
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+#▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▶EJERCICIO #2◀▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+#En Matemática conocemos operaciones de suma, resta, multiplicación y división, 
+#ya sea con números enteros o fraccionarios, 
+#donde se obtiene un nuevo elemento a partir de dos elementos dados. 
+
+#La suma o adicion es la operacion matematica que resulta al reunir en un sola
+#varias cantidades y se lo conoce como suma total.
+#▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+
+#CÓDIGO:
+numero1 = 5
+numero2 = 50
+numero3 = 10
+numero4 = 3
+resultado = numero1 + numero2 + numero3 + numero4
+print("El resultado de la suma es:" , resultado)
+
+#LEY DE LOS SIGNOS  (SUMA Y RESTA)
+
+#+ * + = +
+#+ * - = -
+#- * + = -
+#- * - = +
